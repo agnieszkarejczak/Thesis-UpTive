850098031598add3d0e3b46a469594aa25548a2b
Agnieszka <agix999r@gmail.com>
2021-03-14 21:34:51 +0100

    Fixed naming in backend file structure

--- server/src/test/java/com/example/uptive/UptiveApplicationTests.java
+++ server/src/test/java/com/example/uptive/UptiveApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.uptive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class UptiveApplicationTests {
 
 	@Test
 	void contextLoads() {
