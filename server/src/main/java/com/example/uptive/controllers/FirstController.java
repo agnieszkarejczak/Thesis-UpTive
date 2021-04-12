86f9c3ad252fb4ebaf7827bffbd39d437979b953
Agnieszka <agix999r@gmail.com>
2021-04-12 12:17:59 +0200

    Added database configuration

--- server/src/main/java/com/example/uptive/controllers/FirstController.java
+++ server/src/main/java/com/example/uptive/controllers/FirstController.java
@@ -1,7 +1,6 @@
 package com.example.uptive.controllers;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
